a2bf1ed92042ff7da9c449f04ed08843828d3a11
DrinkingCoder <drinkingcoder@gmail.com>
2019-11-07 13:31:08 +0800

    put json into configs

---
+++ scripts/generate_7scenes.sh
@@ -0,0 +1,7 @@
+../bin/test_tsdf ./7scenes_config/chess.json
+../bin/test_tsdf ./7scenes_config/pumpkin.json
+../bin/test_tsdf ./7scenes_config/fire.json
+../bin/test_tsdf ./7scenes_config/heads.json
+../bin/test_tsdf ./7scenes_config/office.json
+../bin/test_tsdf ./7scenes_config/redkitchen.json
+../bin/test_tsdf ./7scenes_config/stairs.json
