fab9b54ce90555a5fb59bb06e64be9546ba1136b
DrinkingCoder <drinkingcoder@gmail.com>
2020-05-09 11:45:44 +0800

    debug

--- scripts/run_cambridge_all.sh
+++ scripts/run_cambridge_all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-./run_cambridge_raycast.sh "ShopFacade"
-./run_cambridge_raycast.sh "OldHospital"
-./run_cambridge_raycast.sh "KingsCollege"
-./run_cambridge_raycast.sh "GreatCourt"
-./run_cambridge_raycast.sh "StMarysChurch"
+# ./run_cambridge_raycast.sh "ShopFacade"
+# ./run_cambridge_raycast.sh "OldHospital"
+# ./run_cambridge_raycast.sh "KingsCollege"
+# ./run_cambridge_raycast.sh "GreatCourt"
+# ./run_cambridge_raycast.sh "StMarysChurch"
 ./run_cambridge_raycast.sh "Street"
